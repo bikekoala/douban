4dde47bfaaf0a322389d71827c52935ce2b44506
popfeng <popfeng@yeah.net>
2012-07-02 03:10:16 -0700

    set logon tpl.

--- src/library/Douban/Action/Main.php
+++ src/library/Douban/Action/Main.php
@@ -3,8 +3,12 @@ class Douban_Action_Main extends Douban_Action_Abstract
 {
 	public function run()
 	{
-		$this->setResponse($this->auth);
-		$this->tpl('index');
+		if ($this->auth['auth']) {
+			//$this->setResponse($this->auth);
+			echo 'hello';	
+		} else {
+			$this->tpl('logon');
+		}
 		$this->format(Su_Const::FT_HTML);
 	}
 }
